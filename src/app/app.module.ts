7cfc861336cbca459207a77996c9c90b0505525b
LenaikM <lenaik_mohamadi@outlook.fr>
2022-12-20 14:29:06 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserConnexionComponent } from './user-connexion/user-connexion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserConnexionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/user-connexion/user-connexion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-connexion',
+  templateUrl: './user-connexion.component.html',
+  styleUrls: ['./user-connexion.component.scss']
+})
+export class UserConnexionComponent {
+
+}
